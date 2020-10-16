13e49c02bb13e1946ee89685db31ad2a5c20841c
Liam <lon.liam.li@gmail.com>
2020-10-15 23:14:01 -0700

    readline module

--- readline.js
+++ readline.js
@@ -5,11 +5,26 @@ const rl = readline.createInterface({input: process.stdin,
 let num1 = Math.floor((Math.random() * 10) + 1);
 let num2 = Math.floor((Math.random() * 10) + 1);
 
-let anser = num1 + num2;
+let answer = num1 + num2;
 
-rl.question(`What is ${ num1 } + ${ num2 }?`, 
+rl.question(`What is ${ num1 } + ${ num2 }? \n`, 
 (userInput) => {
-    console.log(userInput);
-})
+    if(userInput.trim() == answer){
+        rl.close();
+    } else {
+        rl.setPrompt('Incorrect response please try again \n');
+        rl.prompt();
+        rl.on('line', (userInput)=>{
+            if(userInput.trim() == answer)
+                rl.close();
+            else{
+                rl.setPrompt(`Your answer of ${ userInput } is incorrect try again \n`);
+                rl.prompt();
+            }
+        })
+    }
+});
 
-
+rl.on('close', () => {
+    console.log('Correct!!!!!');
+});
